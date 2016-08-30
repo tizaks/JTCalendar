4df353d6ef1586dc459cac68afff06f61b67b2e8
Tom Izaks <tom@izaks.me>
2016-08-30 17:14:45 +1000

    Add Carthage support

--- JTCalendar/JTCalendar.h
+++ JTCalendar/JTCalendar.h
@@ -5,6 +5,14 @@
 //  Created by Jonathan Tribouharet
 //
 
+//! Project version number for JTCalendar.
+FOUNDATION_EXPORT double JTCalendarVersionNumber;
+
+//! Project version string for JTCalendar.
+FOUNDATION_EXPORT const unsigned char JTCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTCalendar/PublicHeader.h>
+
 #import "JTCalendarManager.h"
 
 #import "JTHorizontalCalendarView.h"
---
+++ JTCalendarTests/JTCalendarTests.m
@@ -0,0 +1,39 @@
+//
+//  JTCalendarTests.m
+//  JTCalendarTests
+//
+//  Created by Tom Izaks on 30/08/2016.
+//  Copyright © 2016 Tom Izaks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JTCalendarTests : XCTestCase
+
+@end
+
+@implementation JTCalendarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
